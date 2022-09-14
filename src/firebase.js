1c7c2e2e0049d613e7f141fd1a1e26ae54903575
Pradeep Raj Nepal <pradeepxwon@gmail.com>
2022-09-14 12:00:54 +0545

    api removed

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import {getAuth} from 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8EyLP0BuI4u5r7LPzOu9TM46IY0v7KDA",
-    authDomain: "firstloginauth.firebaseapp.com",
-    projectId: "firstloginauth",
-    storageBucket: "firstloginauth.appspot.com",
-    messagingSenderId: "714033004613",
-    appId: "1:714033004613:web:7fe60aadcbe8cbd991632c"
+   
 }
 
 const app = initializeApp(firebaseConfig);
